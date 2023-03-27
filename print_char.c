d7f7437fe5a8150a7ebf6697bba6df918594b1ec
KollihTheGuruh <mmike8449@gmail.com>
2023-03-27 14:09:26 -0700

    printf

--- print_char.c
+++ print_char.c
@@ -1,61 +1,17 @@
 #include "main.h"
 
 /**
- * _putchar - writes a character to stdout
- * @c: the character to print
- *
- * Return: 1 on success, -1 on error.
+ * print_char - returns the character
+ * @buff_dest: buffer
+ * @arg: list of arguments
+ * @buff_count: index of buffer pointer
+ * Return: buffer index
  */
-int _putchar(char c)
+int print_char(char *buff_dest, va_list arg, int buff_count)
 {
-	return (write(1, &c, 1));
-}
+	char c = va_arg(arg, int);
 
-/**
- * _printf - produces output according to a format
- * @format: a character string containing zero or more directives
- *
- * Return: the number of characters printed
- */
-int _printf(const char *format, ...)
-{
-	va_list args;
-	int i, count = 0;
+	buff_dest[buff_count] = c;
 
-	va_start(args, format);
-
-	for (i = 0; format && format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			switch (format[i])
-			{
-				case 'c':
-					_putchar(va_arg(args, int));
-					count++;
-					break;
-				case 's':
-					count += write(1, va_arg(args, char *), 0x7fffffff);
-					break;
-				case '%':
-					_putchar('%');
-					count++;
-					break;
-				default:
-					_putchar('%');
-					_putchar(format[i]);
-					count += 2;
-					break;
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			count++;
-		}
-	}
-
-	va_end(args);
-	return (count);
+	return (++buff_count);
 }
